8384f206549c031114d7d46cf9baa552a7c4284f
Bryan <bryan.sia.2022@smu.edu.sg>
2023-09-25 15:18:10 +0800

    Created a DTO class for Event Venue object, Amended the 'setEventsList' portion when inputting venue to allow for mutable list

--- backend/src/main/java/com/seatsecure/backend/services/EventService.java
+++ backend/src/main/java/com/seatsecure/backend/services/EventService.java
@@ -3,9 +3,11 @@ package com.seatsecure.backend.services;
 import java.util.List;
 
 import com.seatsecure.backend.entities.Event;
+import com.seatsecure.backend.entities.EventVenueDTO;
 
 public interface EventService {
-    List<Event> listEvents();
+    List<EventVenueDTO> listEvents();
+    EventVenueDTO getEventVenueById(Long id);
     Event getEventById(Long id);
     Event addEvent(Event e);
     Event updateEvent(Long id, Event newEventInfo);
