02210fdf821fe48723d538b066208b8bddf68d7d
Danish <danishg.2022@scis.smu.edu.sg>
2023-09-26 17:36:04 +0800

    Added Cors configuration

--- backend/src/main/java/com/seatsecure/backend/controllers/UserController.java
+++ backend/src/main/java/com/seatsecure/backend/controllers/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Pattern;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.seatsecure.backend.services.UserService;
 
 @RequestMapping("/api/v1")
 @RestController
+// @CrossOrigin(origins = "*")
 public class UserController {
     private UserService userService;
     private AuthenticationService authService;
--- backend/src/main/java/com/seatsecure/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/seatsecure/backend/security/SecurityConfiguration.java
@@ -1,18 +1,26 @@
 package com.seatsecure.backend.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import com.seatsecure.backend.configs.CorsConfig;
 import com.seatsecure.backend.entities.Permission;
 import com.seatsecure.backend.entities.Role;
 
@@ -28,39 +36,47 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http    
+                .cors(Customizer.withDefaults())
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(r -> r
-                    .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                    .requestMatchers(new AntPathRequestMatcher("/api/v1/auth/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/auth/**")).permitAll()
 
-                    .requestMatchers(new AntPathRequestMatcher("/api/v1/users")).hasRole(Role.ADMIN.name())// the entire page
+                        // .requestMatchers(new
+                        // AntPathRequestMatcher("/api/v1/users")).hasRole(Role.ADMIN.name())// the
+                        // entire page
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/users")).hasRole(Role.ADMIN.name())
+                        // the resepctive HTTP commands
+                        // .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**",
+                        // "HttpMethod.GET")).hasAuthority(Permission.ADMIN_READ.name())
+                        // .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**",
+                        // "HttpMethod.POST")).hasAuthority(Permission.ADMIN_CREATE.name())
+                        // .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**",
+                        // "HttpMethod.PUT")).hasAuthority(Permission.ADMIN_UPDATE.name())
+                        // .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**",
+                        // "HttpMethod.DELETE")).hasAuthority(Permission.ADMIN_DELETE.name())
 
-                    //the resepctive HTTP commands
-                    .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**", "HttpMethod.GET")).hasAuthority(Permission.ADMIN_READ.name())
-                    .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**", "HttpMethod.POST")).hasAuthority(Permission.ADMIN_CREATE.name())
-                    .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**", "HttpMethod.PUT")).hasAuthority(Permission.ADMIN_UPDATE.name())
-                    .requestMatchers(new AntPathRequestMatcher("/api/v1/users/**", "HttpMethod.DELETE")).hasAuthority(Permission.ADMIN_DELETE.name())
+                        // .requestMatchers("/api/v1/admins").hasAnyRole(Role.ADMIN.name())
 
-                    // .requestMatchers("/api/v1/admins").hasAnyRole(Role.ADMIN.name())
+                        // .requestMatchers(HttpMethod.GET,
+                        // "/api/v1/admins/**").hasAuthority(Permission.ADMIN_READ.name())
+                        // .requestMatchers(HttpMethod.POST,
+                        // "/api/v1/admins/**").hasAuthority(Permission.ADMIN_CREATE.name())
+                        // .requestMatchers(HttpMethod.PUT,
+                        // "/api/v1/admins/**").hasAuthority(Permission.ADMIN_UPDATE.name())
+                        // .requestMatchers(HttpMethod.DELETE,
+                        // "/api/v1/admins/**").hasAuthority(Permission.ADMIN_DELETE.name())
+                        .anyRequest().authenticated())
 
-                    // .requestMatchers(HttpMethod.GET, "/api/v1/admins/**").hasAuthority(Permission.ADMIN_READ.name())
-                    // .requestMatchers(HttpMethod.POST, "/api/v1/admins/**").hasAuthority(Permission.ADMIN_CREATE.name())
-                    // .requestMatchers(HttpMethod.PUT, "/api/v1/admins/**").hasAuthority(Permission.ADMIN_UPDATE.name())
-                    // .requestMatchers(HttpMethod.DELETE, "/api/v1/admins/**").hasAuthority(Permission.ADMIN_DELETE.name())
-                    .anyRequest().authenticated()
-                )
-                
-                .sessionManagement((sessionManagement) ->
-                    sessionManagement
+                .sessionManagement((sessionManagement) -> sessionManagement
                         // .sessionConcurrency((sessionConcurrency) ->
-                        //         sessionConcurrency
-                        //                 .maximumSessions(1)
-                        //                 .expiredUrl("/login?expired")
-                        //                 )
+                        // sessionConcurrency
+                        // .maximumSessions(1)
+                        // .expiredUrl("/login?expired")
+                        // )
 
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
@@ -69,6 +85,18 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
                 
+
         return http.build();
     }
+
+
+    // private Customizer<CorsConfigurer<HttpSecurity>> withDefaults() {
+    //     CorsConfiguration configuration = new CorsConfiguration();
+    //     configuration.setAllowedOrigins(Arrays.asList("*"));
+    //     configuration.setAllowedMethods(Arrays.asList("*"));
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     source.registerCorsConfiguration("/**", configuration);
+    //     return (Customizer<CorsConfigurer<HttpSecurity>>) source;
+    // }
+
 }
