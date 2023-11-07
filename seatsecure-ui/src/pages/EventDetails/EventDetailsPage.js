ab770c0759a49c1d45a37fe5ffeb43d72de4e371
Danish <danishg.2022@scis.smu.edu.sg>
2023-11-07 14:48:26 +0800

    Cat Selection, Event Details Page: Map Data from backend

--- seatsecure-ui/src/pages/EventDetails/EventDetailsPage.js
+++ seatsecure-ui/src/pages/EventDetails/EventDetailsPage.js
@@ -4,17 +4,21 @@ import './EventsDetails.css';
 import Navbar from '../../components/Navbar';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Faq from 'react-faq-component';
-import useUser from '../../hooks/useUser'
+import useUser from '../../hooks/useUser';
+
 
 const EventDetailsPage = (props) => {
 
+  const navigate = useNavigate();
   const user = useUser();
   var IDValue = parseInt(user.userID.ID);
+
   const location = useLocation();
   const state = location.state;
+  console.log(state);
+  
   const title = "SAVE-THE-DAY CONCERT BY COLDPLAY 2023"
   const description = "Following the spectacular sellos newly announced January and February 2024 dates in Asia, the band have today announced additional dates as part of their record-breaking Music Of The Spheres World Tour , Already making history for announcing an unprecedented four-show run at Singapore’s largest venue, Coldplay will now be the first act ever to play five nights at Singapore’s National Stadium. The band also broke Singapore’s record for most tickets sold by an artist in a single day, surpassing 200,000.";
-  const navigate = useNavigate();
 
   const data = {
     title: "",
@@ -34,13 +38,20 @@ const EventDetailsPage = (props) => {
     ]
   }
 
-
   const handlepurchase = () => {
-    if (isNaN(IDValue)) {
-      alert('Please login to continue.');
+
+    console.log(IDValue);
+
+    if( isNaN( IDValue)) {
+      alert('Please login to continue!');
     }
     else {
-      navigate('/CatSelection')
+
+      navigate(
+        '/CatSelection',
+        {state:state}
+    
+      ); //state = {Event}
     }
   };
 
@@ -53,11 +64,11 @@ const EventDetailsPage = (props) => {
           <div className="event-div-title">{state.event.eventName}
           </div>
           <div className="event-picture">
-            <div className="event-date">Date: {state.event.startDate} to {state.event.endDate}</div>
-            <div className="event-location">Location: {state.venue.venueName}</div>
+            <div className="event-date">Date:{state.event.startDate} to {state.event.endDate}</div>
+            <div className="event-location">Location:{state.venue.venueName}</div>
           </div>
           <div className="event-button">
-            <input type="button" onClick={handlepurchase} value="Purchase" />
+            <input type="button" onClick ={handlepurchase} value="Purchase" />
           </div>
           <div className="event-div-text">Event Details</div>
           <div className="event-divider-line"></div>
